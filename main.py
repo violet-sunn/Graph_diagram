6727f63fc96f1a0e7038ce6e1a813893f9b45df6
violet-sunn <mojocorgi@gmail.com>
2022-07-30 15:09:59 +0300

    node testing & test diagram

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ filename = 'json.json'
 with open(filename) as f:
     d = json.load(f)
 
-parents = dict.fromkeys(['id'], [])
+parents = []
+children = []
 
 for items in d['nodes']:
     if 'children' in items:
-        parents['id'].append(items.get('id'))
+        parents.append(items.get('id'))
+    else:
+        children.append(items.get('id'))
 
 print(parents)
+print(children)
 
-'''g = graphviz.Digraph('G', filename='hello.gv')
+g = graphviz.Digraph('G', filename='diagram.gv')
 
-g.edge('Hello', 'World')
+parent_node_list = []
+children_node_list = []
 
-g.view()'''
+for n in range(parents.__len__()):
+    g.node('pn' + str(n), 'pn ' + str(parents[n]))
+    parent_node_list.append('pn' + str(n))
+
+for n in range(children.__len__()):
+    g.node('cn' + str(n), 'cn ' + str(children[n]))
+    children_node_list.append('cn' + str(n))
+
+g.node('test', 'TEST_NODE')
+for nodes in parent_node_list:
+    g.edge(nodes, 'test')
+
+g.view()
